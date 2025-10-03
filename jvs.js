28f1ab006acd76d2662a6f2379e22c8d86ed5b4c
GiaBao6769 <hgiabao6729@gmail.com>
2025-10-03 21:46:43 +0700

    Add files via upload

--- jvs.js
+++ jvs.js
@@ -1,16 +1,30 @@
-var menu = document.getElementById("infoAppendixDiv")
 function showMenu(){
+    const menu = document.getElementById("infoAppendixDiv");
     menu.style.right = "0";
     
 }
 
 function hideMenu(){
+    const menu = document.getElementById("infoAppendixDiv");
     menu.style.right = "-40vw";
 }
 
 
 fetch("template.html")
-  .then(res => res.text())
-  .then(data => {
-    document.querySelector("footer").innerHTML = data;
+    fetch("template.html")
+    .then(res => res.text())
+    .then(data => {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(data, "text/html");
+
+    const Footer = doc.querySelector("footer");
+    document.querySelector("footer").replaceWith(Footer);
+
+    const navBar = doc.querySelector(".navBar");
+    document.querySelector(".navBar").replaceWith(navBar);
+
+    const banner = doc.querySelector(".banner");
+    document.querySelector(".banner").replaceWith(banner);
 });
+
+
